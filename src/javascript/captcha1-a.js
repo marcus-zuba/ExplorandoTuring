7ebf26138244366e79a0af4983e46b0324ba0dd5
Marcus Vinicius <49821573+marcus-zuba@users.noreply.github.com>
2021-01-19 15:14:13 -0300

    changing hints to explanation

--- src/javascript/captcha1-a.js
+++ src/javascript/captcha1-a.js
@@ -1,18 +1,3 @@
-let x = document.querySelector("#x");
-let y = document.querySelector("#y");
-let xMove = document.querySelector("#Xmovement");
-let yMove = document.querySelector("#Ymovement");
-
-let divMaquina = document.querySelector("#MAQUINA");
-
-
-window.addEventListener("mousemove", function (e) {
-  x.innerHTML = `${e.clientX}`
-  y.innerHTML = `${e.clientY}`
-  xMove.innerHTML = `${e.movementX}`
-  yMove.innerHTML = `${e.movementY}`
-});
-
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
